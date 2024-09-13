3d13ee698221442a89705e3f4d5cd3bf36cb410c
itfeelsharsh <hello@itfeelsharsh.dev>
2024-09-13 23:21:24 +0530

    Final Update

---
+++ src/serviceWorkerRegistration.js
@@ -0,0 +1,32 @@
+// src/serviceWorkerRegistration.js
+
+// This is where the service worker registration happens
+export function register() {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+        
+        navigator.serviceWorker
+          .register(swUrl)
+          .then((registration) => {
+            console.log('Service Worker registered: ', registration);
+          })
+          .catch((registrationError) => {
+            console.log('Service Worker registration failed: ', registrationError);
+          });
+      });
+    }
+  }
+  
+  export function unregister() {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.ready
+        .then((registration) => {
+          registration.unregister();
+        })
+        .catch((error) => {
+          console.error(error.message);
+        });
+    }
+  }
+  
